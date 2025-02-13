7263b990d6d67611a96802feca5e490c34c0e374
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-13 15:46:27 +0100

    [import]Navbar to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,12 @@
+import Navbar from "./Navbar";
+
+
 export default function Header() {
     return (<header>
         <figure>
             <img src="./src/assets/img/dc-logo.png" alt="" />
         </figure>
-        <nav>
-            <ul>
-                <li><a href="">CHARACTERS</a></li>
-                <li><a href="" className="active">COMICS</a></li>
-                <li><a href="">MOVIES</a></li>
-                <li><a href="">TV</a></li>
-                <li><a href="">GAMES</a></li>
-                <li><a href="">COLLECTIBLES</a></li>
-                <li><a href="">VIDEOS</a></li>
-                <li><a href="">FANS</a></li>
-                <li><a href="">NEWS</a></li>
-                <li><a href="">SHOP</a></li>
-            </ul>
-        </nav>
+        <Navbar />
+
         </header>);
 }
