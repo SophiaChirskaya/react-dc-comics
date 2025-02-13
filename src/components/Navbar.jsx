4e2221c1fc8599265d24a974e331a1d4312d87ad
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-13 20:04:00 +0100

    [feat]props pass

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,25 @@
 export default function Navbar(props) {
-    const links = [
-        { id: 1, text: 'CHARACTERS', url: '#', current: false },
-        { id: 2, text: 'COMICS', url: '#', current: true },
-        { id: 3, text: 'MOVIES', url: '#', current: false },
-        { id: 4, text: 'TV', url: '#', current: false },
-        { id: 5, text: 'GAMES', url: '#', current: false },
-        { id: 6, text: 'COLLECTIBLES', url: '#', current: false },
-        { id: 7, text: 'VIDEOS', url: '#', current: false },
-        { id: 8, text: 'FANS', url: '#', current: false },
-        { id: 9, text: 'NEWS', url: '#', current: false },
-        { id: 10, text: 'SHOP', url: '#', current: false },
-    ];
+    // const links = [
+    //     { id: 1, text: 'CHARACTERS', url: '#', current: false },
+    //     { id: 2, text: 'COMICS', url: '#', current: true },
+    //     { id: 3, text: 'MOVIES', url: '#', current: false },
+    //     { id: 4, text: 'TV', url: '#', current: false },
+    //     { id: 5, text: 'GAMES', url: '#', current: false },
+    //     { id: 6, text: 'COLLECTIBLES', url: '#', current: false },
+    //     { id: 7, text: 'VIDEOS', url: '#', current: false },
+    //     { id: 8, text: 'FANS', url: '#', current: false },
+    //     { id: 9, text: 'NEWS', url: '#', current: false },
+    //     { id: 10, text: 'SHOP', url: '#', current: false },
+    // ];
 
 
     return (<nav>
             <ul>
-                {links.map((link) => (
+                {props.navLinks.map((link) => (
                     <li key={link.id}>
                         <a href={link.url} className={link.current ? 'active' : ''}>{link.text}</a>
                     </li>
                 ))}
             </ul>
     </nav>);
-}
+};
