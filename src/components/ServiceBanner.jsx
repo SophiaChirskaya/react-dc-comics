d4da15e21bc310ac9affc7524336d54a9198564e
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-13 06:17:16 +0100

    [feat]banner

---
+++ src/components/ServiceBanner.jsx
@@ -0,0 +1,29 @@
+export default function ServiceBanner(){
+    return(
+        <div className="services-wrapper">
+            <section className="services">
+                <div className="service">
+                    <img className="services-img" src="./src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+                </div>
+                <div className="service">
+                    <img className="services-img" src="./src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+                </div>
+                <div className="service">
+                    <img className="services-img" src="./src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+                </div>
+                <div className="service">
+                    <img className="services-img" src="./src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+                </div>
+                <div className="service">
+                    <img className="services-img" src="./src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+                </div>
+
+            </section>
+        </div>
+    )
+}
