4ad5cb97a2fcf31ae223552103380687e890b540
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-14 18:12:23 +0100

    [feat]productsList.jsx

---
+++ src/components/products/ProductsList.jsx
@@ -0,0 +1,11 @@
+export default function ProductsList() {
+    return (<main className="main-content">
+        <div className="content-wrapper">
+            <section className="content">
+                <h2> Content goes here </h2>
+            </section>
+        </div>
+        <ServiceBanner />
+         </main>
+         );
+}
